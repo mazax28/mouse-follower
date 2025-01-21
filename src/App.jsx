de061be33ce5299c673e7ce3d290f67d6421dd57
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-01-21 11:03:19 -0300

    feat: implement mouse tracking feature with toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  
+  const handleClick = () => {
+
+    setEnabled((prevEnabled) => !prevEnabled)
+  }
+
+  useEffect(()=>{
+    const handleMove = (event) =>{
+      const { clientX, clientY } = event;
+
+      setPosition({ x: clientX, y: clientY })
+    }
+    if(enabled){
+      window.addEventListener('mousemove', handleMove)
+    }
+    return () => {
+      window.removeEventListener('mousemove', handleMove)
+    }
+
+  },[enabled] )
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+     <button onClick={handleClick} >{enabled ? 'Dejar se seguir el puntero':'Seguir Puntero'}</button>
     </>
   )
 }
